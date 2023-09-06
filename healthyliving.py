22f4699c08f7530c298c2aa0056e70d6d8a9a8a0
Big-Jeans <143380936+Big-Jeans@users.noreply.github.com>
2023-09-06 18:26:03 +0800

    layout

--- healthyliving.py
+++ healthyliving.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '4d1b6a8662eff78fccab9ee6ca898d89'
 
 @app.route("/")
-def hello():
+def index():
     image_veg = url_for('static', filename='images/Picture of Vegetable (1).png')
     image_about = url_for('static', filename='images/About Picture.png')
     image_sleep = url_for('static', filename='images/Sleeping Picture.png')
-    return render_template("index.html", image_veg = image_veg, image_about = image_about, image_sleep = image_sleep)
+    return render_template("index.html", title = index, image_veg = image_veg, image_about = image_about, image_sleep = image_sleep)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -17,12 +17,12 @@ def register():
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('login'))
-    return render_template("register.html", form = form)
+    return render_template("register.html", title = register, form = form)
 
 @app.route("/login")
 def login():
     form = LoginForm()
-    return render_template("login.html", form = form)
+    return render_template("login.html", title = login, form = form)
 
 if __name__ == '__main__':
     app.run(debug = True)
